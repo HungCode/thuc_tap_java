ab8006f0fb1567e7d8f8cba2b12ca937dad9cc8d
tnv.9119 <phieuphieuit@gmail.com>
2018-05-12 17:26:34 +0700

    apilocation

--- Thuc_hanh_tool/bai3/src/main/java/spark/bai3/App.java
+++ Thuc_hanh_tool/bai3/src/main/java/spark/bai3/App.java
@@ -1,11 +1,6 @@
 package spark.bai3;
 
-import static spark.Spark.get;
-
-import java.util.ArrayList;
-
-import spark.Spark;
-
+import static spark.Spark.*;
 
 /**
  * Hello world!
@@ -13,43 +8,14 @@ import spark.Spark;
  */
 public class App {
 
-	public static int[] sang_nguyen_to(int n) {
-
-		int[] snt = new int[n + 1];
-
-		int i, j;
-		for (i = 1; i <= n; i++)
-			snt[i] = 1;
-		snt[1] = 0;
-		i = 2;
-		double l = Math.sqrt(n);
-		while (i <= l) {
-			while (snt[i] == 0)
-				i++;
-			for (j = 2; j <= n / i; j++)
-				snt[i * j] = 0;
-			i++;
-		}
-		return snt;
-	}
-	
-	public static ArrayList<Integer> so_nguyen_to(int n) {
-		int[] snt = sang_nguyen_to(n);
-		ArrayList<Integer> kq = new ArrayList<>();
-		for (int i=0;i<=n;i++) {
-			if(snt[i]!=0)
-				kq.add(i);
-		}
-		return kq;
-	}
 	public static void main(String[] args) {
-//		System.out.println(so_nguyen_to(100));
 
-		get("/prime", (req, res) -> {
-			int Max = Integer.parseInt(req.queryParams("q"));
-			System.out.println(Max);
-			ArrayList<Integer> arr = so_nguyen_to(Max);
-			return arr;
+		get("/iplocation/:ip", (req, res) -> {
+			String ip = req.params(":ip");
+			res.redirect("http://ip-api.com/json/" + ip);
+			return "";
 		});
+
+
 	}
 }
